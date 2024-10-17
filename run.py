d6f3810001f572dacf81d97bf4fa5d65bdbb763b
Naveed Naseem <naveed.naseem@nwas.nhs.uk>
2024-10-17 12:34:02 +0000

    def display instructions and random created

--- run.py
+++ run.py
@@ -1,8 +1,31 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-def test():
-    print("hello")
 
+import random
 
-test()
+def display_instructions():
+    """
+    Instructions for the game read in from the
+    instructions file and displayed.
+    """  
+    file = open("instructions.txt", 'r')
+    instructions = file.read()
+    print(instructions)
+    file.close()
+
+def choose_random_word():
+    """
+    Read in words from words file and assign to a 
+    list using the new line as split. 
+    Randomly select a word from this list
+    """
+    file = open("words.txt", 'r')
+    words = file.read()
+    word_list = words.split("\n")
+    file.close()
+
+    random_word = random.choice(word_list)
+    print(random_word)
+    print(len(random_word))
+
+    
+#display_instructions()
+choose_random_word()
